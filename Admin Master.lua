147a509655c0cd2104341dae132cf88ca3a06c92
AdminMasterofficial <123615199+AdminMasterofficial@users.noreply.github.com>
2023-01-26 12:56:20 +0800

    Update Admin Master.lua

--- Admin Master.lua
+++ Admin Master.lua
@@ -1,26 +1,40 @@
-local Library = loadstring(game:HttpGet("htps://pastebin.com/rawt/vff1bQ9F"))()
-local Window = Library.CreateLib("Admin Master", "Red")
+if game.PlaceId == 155615604 then
 
-local MainTab = Window:NewTab("Main")
-local MainTabSection = MainTab:NewSection("Main")
+    local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/xHeptc/Kavo-UI-Library/main/source.lua"))()
+    local Window = Library.CreateLib("Admin Master (Prison Life)", "Ocean")
 
-MainTab:NewButton("Inf Jumps", "Enables Inf Jumps", function()
-    local InfiniteJumpEnabled = true
-game:GetService("UserInputService").JumpRequest:connect(function()
-	if InfiniteJumpEnabled then
-		game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
-	end
-end)
-end)
+    local Main = Window:NewTab("Main")
+    local MainSection = Main:NewSection("Main")
+    
+    MainSection:NewToggle("Fov", "Changes Fov", function(state)
+        if state then
+            game.Workspace.CurrentCamera.FieldOfView = 120
+        else
+            game.Workspace.CurrentCamera.FieldOfView = 80
+        end
+    end)
 
-MainTab:NewToggle("Fov", "Changes Fov", function(state)
-    if state then
-        game.Workspace.CurrentCamera.FieldOfView = 120
-    else
-        game.Workspace.CurrentCamera.FieldOfView = 80
-    end
-end)
+    MainSection:NewSlider("WalkSpeed", "Makes your faster", 250, 120, function(v)
+        game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
+    end)
 
-MainTab:NewSlider("WalkSpeed", "Makes your faster", 250, 120, function(v)
-    game.Players.LocalPlayer.Character.Humanoid.WalkSpeed = v
-end)
+    MainSection:NewToggle("Infinite Jump", "Toggle for infinite jump", function(state)
+        if state then
+            local InfiniteJumpEnabled = true
+            game:GetService("UserInputService").JumpRequest:connect(function()
+                if InfiniteJumpEnabled then
+                    game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+                end
+            end)
+            print("Infinite Jump: ON")
+        else
+            InfiniteJumpEnabled = false
+            game:GetService("UserInputService").JumpRequest:connect(function()
+                if InfiniteJumpEnabled then
+                    game:GetService"Players".LocalPlayer.Character:FindFirstChildOfClass'Humanoid':ChangeState("Jumping")
+                end
+            end)
+            print("Infinite Jump: OFF")
+        end
+    end)
+end
