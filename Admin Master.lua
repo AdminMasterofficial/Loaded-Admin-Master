dd2a4ecb94be1b18fbcd2f48fae2b472b81c1439
AdminMasterofficial <123615199+AdminMasterofficial@users.noreply.github.com>
2023-01-26 14:18:02 +0800

    Update Admin Master.lua

--- Admin Master.lua
+++ Admin Master.lua
@@ -74,38 +74,6 @@ if game.PlaceId == 155615604 then
     local ServerClient = Window:NewTab("Server Client")
     local ServerClientSection = ServerClient:NewSection("Server Client")
 
-    ServerClientSection:NewButton("Server Crash", "Crashing Server", function(FireGun, target)
-        local Gun = "Remington 870"
-        local Player = game.Players.LocalPlayer.Name
-        game.workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.giver[Gun].ITEMPICKUP)
-        for i,v in pairs(game.Players[Player].Backpack:GetChildren()) do
-            if v.name == (Gun) then
-                v.Parent = game.Players.LocalPlayer.Character
-            end
-        end
-        Gun = game.Players[Player].Character[Gun]
-        game.Players.LocalPlayer.Character:FindFirstChildOfClass("Humanoid"):UnequipTools()
-
-        function FireGun()
-            coroutine.resume(coroutine.create(function()
-                local bulletTable = {}
-                table.insert(bulletTable, {
-                    Hit = target,
-                    Distance = 100,
-                    Cframe = CFrame.new(0,1,1),
-                    RayObject = Ray.new(Vector3.new(0.1,0.2), Vector3.new(0.3,0.4))
-                })
-                game.ReplicatedStorage.ShootEvent:FireServer(bulletTable, Gun)
-                wait()
-            end))
-        end
-        while game:GetService("RunService").Stepped:wait() do
-            for count = 0, 10, 10 do
-                FireGun()
-            end
-        end
-    end)
-
     ServerClientSection:NewButton("Open Gate", "Makes the gate open for you", function()
         Workspace.Remote.ItemHandler:InvokeServer(workspace.Prison_ITEMS.buttons["Prison Gate"]["Prison Gate"])
  end)
